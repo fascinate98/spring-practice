b9a62059c1d0145e5dacc01224e52c3f178946ad
fascinate98 <fascinate982@naver.com>
2022-01-27 16:54:11 +0900

    fileupload

--- fileupload/src/main/java/com/poscoict/fileupload/service/FileUploadService.java
+++ fileupload/src/main/java/com/poscoict/fileupload/service/FileUploadService.java
@@ -1,24 +1,42 @@
 package com.poscoict.fileupload.service;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class FileUploadService {
+	private static String SAVE_PATH = "/upload-images";
 	
 	public String restore(MultipartFile multipartfile) {
+		
 		String url = null;
 		
-		if(multipartfile.isEmpty()) {
-			return url;
+		try {
+			
+			
+			
+			if(multipartfile.isEmpty()) {
+				return url;
+			}
+			
+			String originfilename =  multipartfile.getOriginalFilename();
+			long filesize = multipartfile.getSize();
+			
+			System.out.println("!@#!@#!@#!@#!@#!@#!@#!#!#!@#!#!@#!@#!@#!@#" + originfilename);
+			System.out.println("!@#!@#!@#!@#!@#!@#!@#!#!#!@#!#!@#!@#!@#!@#" + filesize);
+		
+			
+			byte[] data =  multipartfile.getBytes();
+			OutputStream os = new FileOutputStream(SAVE_PATH + "");
+			
+		} catch(IOException ex) {
+			//fileuploadexception으로 만들어서 메ㅐ핑
+			throw new RuntimeException("file upload errrrr.. :" + ex);
 		}
-		
-		String originfilename =  multipartfile.getOriginalFilename();
-		long filesize = multipartfile.getSize();
-		
-		System.out.println("!@#!@#!@#!@#!@#!@#!@#!#!#!@#!#!@#!@#!@#!@#" + originfilename);
-		System.out.println("!@#!@#!@#!@#!@#!@#!@#!#!#!@#!#!@#!@#!@#!@#" + filesize);
-	
 		return url;
 	}
 
