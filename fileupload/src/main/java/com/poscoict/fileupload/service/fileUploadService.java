94c4f2cf8b0de40b72606c3cfff74f5746c6a763
fascinate98 <fascinate982@naver.com>
2022-01-27 15:53:11 +0900

    fileupload 01

---
+++ fileupload/src/main/java/com/poscoict/fileupload/service/fileUploadService.java
@@ -0,0 +1,13 @@
+package com.poscoict.fileupload.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class fileUploadService {
+	
+	public String restore(MultipartFile multipartfile) {
+		return null;
+	}
+
+}
